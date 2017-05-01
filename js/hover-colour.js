b0c5e07c57c0c1e4249a21e053ffe7e757d0cdb2
Nart Barileva <barilevanart@gmail.com>
2017-04-30 21:14:55 -0400

    Uploaded all files
    Hopefully folders uploaded properly

---
+++ js/hover-colour.js
@@ -0,0 +1,39 @@
+$('.content-section a:nth-child(1) .content-section-card').hover(function() {
+    $('body').css('background-color', '#e52d27');
+    $('.content-section a:nth-child(1) p').css('color', '#e52d27');
+    $('.content-section a:nth-child(1) path').css('fill', '#e52d27');
+}, function() {
+    $('body').css('background-color', '');
+    $('.content-section a:nth-child(1) p').css('color', '');
+    $('.content-section a:nth-child(1) path').css('fill', '');
+});
+
+$('.content-section a:nth-child(2) .content-section-card').hover(function() {
+    $('body').css('background-color', '#ff0084');
+    $('.content-section a:nth-child(2) p').css('color', '#ff0084');
+    $('.content-section a:nth-child(2) path').css('fill', '#ff0084');
+}, function() {
+    $('body').css('background-color', '');
+    $('.content-section a:nth-child(2) p').css('color', '');
+    $('.content-section a:nth-child(2) path').css('fill', '');
+});
+
+$('.content-section a:nth-child(3) .content-section-card').hover(function() {
+    $('body').css('background-color', '#00ab6b');
+    $('.content-section a:nth-child(3) p').css('color', '#00ab6b');
+    $('.content-section a:nth-child(3) path').css('fill', '#00ab6b');
+}, function() {
+    $('body').css('background-color', '');
+    $('.content-section a:nth-child(3) p').css('color', '');
+    $('.content-section a:nth-child(3) path').css('fill', '');
+});
+
+$('.content-section a:nth-child(4) .content-section-card').hover(function() {
+    $('body').css('background-color', '#1769ff');
+    $('.content-section a:nth-child(4) p').css('color', '#1769ff');
+    $('.content-section a:nth-child(4) path, rect').css('fill', '#1769ff');
+}, function() {
+    $('body').css('background-color', '');
+    $('.content-section a:nth-child(4) p').css('color', '');
+    $('.content-section a:nth-child(4) path, rect').css('fill', '');
+});
